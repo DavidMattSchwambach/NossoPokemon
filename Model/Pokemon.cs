23709407301b988dd6a830660dc150f53440b5e8
DavidMattSchwambach <davidschwambach@hotmail.com>
2019-06-28 13:00:40 -0300

    Adicionado PokemonRepository, Cadastro de Pokémons e adicionado apagar e editar nas categorias.

---
+++ Model/Pokemon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Pokemon
+    {
+        public int Id;
+        public int IdCategoria;
+
+        public Categoria categoria;
+
+        public string Nome;
+
+        public Categoria Categoria { get; set; }
+    }
+}
